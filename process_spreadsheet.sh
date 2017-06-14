87558bc502078bd3078a9b124ece092315c0ea9f
ngopal <nikhilgopal@gmail.com>
2017-06-14 15:05:34 -0700

    removed excess files

--- process_spreadsheet.sh
+++
@@ -1,9 +0,0 @@
-cat mlnotes.json | \
-sed 's/Assumes Normality (Gaussian)/assumes_normal/' | \
-sed 's/Assumes Homoscedasity/assumes_homosked/' | \
-sed 's/Assumes Linearity/assumes_linear/' | \
-sed 's/Assumes Indepent Input Variables/assumes_indep/' | \
-sed 's/Robust to Missing Data/robust_NA/' | \
-sed 's/Robust to large P to N/robust_smallNlargeP/' | \
-less
-
